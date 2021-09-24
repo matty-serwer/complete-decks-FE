d9dce7ad746d5218a9569c047ebd4fe7d7e33744
Matthew Serwer <Matthew.Serwer@gmail.com>
2021-09-24 16:10:54 -0400

    Adds background color to every page. Adds cart 'still-need' logic and links

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Container, Row, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import CartContext from '../context/Context';
 // components
 import NavbarComponent from './Navbar';
@@ -16,6 +17,9 @@ const Cart: React.FC<ICartProps> = props => {
   const deckComplete = cartContext.deckComplete;
 
   const [total, setTotal] = useState(0);
+  const [deckInCart, setDeckInCart] = useState(false);
+  const [trucksInCart, settrucksInCart] = useState(false);
+  const [wheelsInCart, setWheelsInCart] = useState(false);
   const [completeDeck, setCompleteDeck] = useState(false);
 
 
@@ -24,14 +28,13 @@ const Cart: React.FC<ICartProps> = props => {
       cartItems.reduce((acc, _item) => acc + Number(_item.price), 0)
     )
 
-    // let decksInCart = cartItems.some((_item) => _item.category === "decks");
-    // let trucksInCart = cartItems.some((_item) => _item.category === "trucks");
-    // let wheelsInCart = cartItems.some((_item) => _item.category === "wheels");
+    let deck = cartItems.some((_item) => _item.category === "decks");
+    let trucks = cartItems.some((_item) => _item.category === "trucks");
+    let wheels = cartItems.some((_item) => _item.category === "wheels");
 
-    // if (decksInCart && trucksInCart && wheelsInCart) {
-    //   setCompleteDeck(true);
-    // }
-
+    setDeckInCart(deck);
+    settrucksInCart(trucks);
+    setWheelsInCart(wheels);
 
   }, [cartItems])
 
@@ -56,11 +59,38 @@ const Cart: React.FC<ICartProps> = props => {
           <div className="total-container">
             <p className="total"> Total: ${total}</p>
           </div>
-          <div className="button-container">
-            <Button variant="outline-primary" className="reg-button shop-button">Save Board</Button>
-            <Button variant="outline-primary" className="reg-button shop-button">Checkout</Button>
+          {cartItems.length === 3 ? (
+            <div className="button-container">
+              <Button variant="outline-primary" className="reg-button shop-button">Save Board</Button>
+              <Button variant="outline-primary" className="reg-button shop-button">Checkout</Button>
+            </div>
+          ) : (
+            null
+          )}
+          <div className="still-need">
+            {cartItems.length < 3 ? (
+              <span>Looks like you still need: </span>
+            ) : (
+              null
+            )}
+            <div className="need-items">
+              {!deckInCart ? (
+                <Link to='/items?category=decks' className="cart-link">a deck &nbsp;&nbsp;&nbsp;&nbsp;</Link>
+              ) : (
+                null
+              )}
+              {!trucksInCart ? (
+                <Link to='/items?category=trucks' className="cart-link">trucks &nbsp;&nbsp;&nbsp;&nbsp;</Link>
+              ) : (
+                null
+              )}
+              {!wheelsInCart ? (
+                <Link to='/items?category=wheels' className="cart-link">wheels &nbsp;&nbsp;&nbsp;&nbsp;</Link>
+              ) : (
+                null
+              )}
+            </div>
           </div>
-
         </div>
       </div>
     </div>
